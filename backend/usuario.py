f154a8a8fb4346f232fd8fd1a64756fbc95a2df7
JoaquinCamino12 <131208171+JoaquinCamino12@users.noreply.github.com>
2023-09-13 12:30:27 -0300

    usuario.py modificacion
    se agrego domicilio

--- backend/usuario.py
+++ backend/usuario.py
@@ -1,11 +1,12 @@
 class Usuario:
-    def __init__(self, id, nombre, apellido, correo, contrasena, dni):
+    def __init__(self, id, nombre, apellido, correo, contrasena, dni, domicilio):
         self.id = id
         self.nombre = nombre
         self.apellido = apellido
         self.correo = correo
         self.contrasena = contrasena
         self.dni = dni
+        self.domicilio = domicilio
 #Agregar logica en sprint 2
     def registrar(self):
         pass
