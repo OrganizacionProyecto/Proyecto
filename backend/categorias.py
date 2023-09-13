29e85bde379c70ae643493e6280e5cc446479bd6
JoaquinCamino12 <joaquincamino09@gmail.com>
2023-09-13 12:23:47 -0300

    Modularizacion sprint 1

---
+++ backend/categorias.py
@@ -0,0 +1,7 @@
+class Categoria:
+    def __init__(self, id, nombre):
+        self.id = id
+        self.nombre = nombre
+# Lógica para obtener productos por categoría en el sprint 2
+    def obtener_productos_por_categoria(self, categoria_id):
+        pass
---
+++ backend/usuario.py
@@ -0,0 +1,14 @@
+class Usuario:
+    def __init__(self, id, nombre, apellido, correo, contrasena, dni):
+        self.id = id
+        self.nombre = nombre
+        self.apellido = apellido
+        self.correo = correo
+        self.contrasena = contrasena
+        self.dni = dni
+#Agregar logica en sprint 2
+    def registrar(self):
+        pass
+
+    def iniciar_sesion(self):
+        pass
