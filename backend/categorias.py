527591ed2a9da48d8b25ed7936241d77541dbcbe
JoaquinCamino12 <131208171+JoaquinCamino12@users.noreply.github.com>
2023-09-20 18:42:28 -0300

    Delete backend directory

--- backend/categorias.py
+++
@@ -1,7 +0,0 @@
-class Categoria:
-    def __init__(self, id, nombre):
-        self.id = id
-        self.nombre = nombre
-# Lógica para obtener productos por categoría en el sprint 2
-    def productos_categoria(self, categoria_id):
-        pass
