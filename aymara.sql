5c982bdacb7135a5b894ec72a0c5a464f6aa83ec
Andres <andres-rio3@hotmail.com>
2023-09-21 09:47:40 -0300

    Se Modifico el codigo para optimizar el funcionamiento de la base de datos y se inserto algunas explicaciones

--- aymara.sql
+++ aymara.sql
@@ -1,232 +1,80 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 16-09-2023 a las 16:53:07
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Schema aymara
+CREATE SCHEMA IF NOT EXISTS `aymara` DEFAULT CHARACTER SET utf8 ;
+USE `aymara` ;
+
+-- Table `aymara`.`categoria`
+CREATE TABLE IF NOT EXISTS `aymara`.`categoria` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+-- Table `aymara`.`producto`
+CREATE TABLE IF NOT EXISTS `aymara`.`producto` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NOT NULL,
+  `descripcion` TEXT NOT NULL,
+  `precio` DECIMAL(10, 2) NOT NULL,
+  `imagen` VARCHAR(255) NULL,
+  `stock` INT(11) NOT NULL,
+  `categoria_id` INT(11) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `categoria_id_idx` (`categoria_id` ASC),
+  CONSTRAINT `categoria_id`
+    FOREIGN KEY (`categoria_id`)
+    REFERENCES `aymara`.`categoria` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB;
+
+###xx_
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- Table `aymara`.`cliente`
+CREATE TABLE IF NOT EXISTS `aymara`.`cliente` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NOT NULL,
+  `apellido` VARCHAR(255) NOT NULL,
+  `correo` VARCHAR(255) NOT NULL,
+  `dni` INT(11) NOT NULL,
+  `contrasenia` VARCHAR(255) NOT NULL,
+  `domicilio` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
 
---
--- Base de datos: `aymara`
---
+-- Table `aymara`.`pedido`
+CREATE TABLE IF NOT EXISTS `aymara`.`pedido` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT(11) NOT NULL,
+  `estado` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_usuario_idx` (`id_usuario` ASC),
+  CONSTRAINT `id_usuario`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `aymara`.`cliente` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 
--- --------------------------------------------------------
+-- Table `aymara`.`pedido_producto`
+CREATE TABLE IF NOT EXISTS `aymara`.`pedido_producto` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `pedido_id` INT(11) NULL,
+  `producto_id` INT(11) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `pedido_id_idx` (`pedido_id` ASC),
+  INDEX `producto_id_idx` (`producto_id` ASC),
+  CONSTRAINT `pedido_id`
+    FOREIGN KEY (`pedido_id`)
+    REFERENCES `aymara`.`pedido` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `producto_id`
+    FOREIGN KEY (`producto_id`)
+    REFERENCES `aymara`.`producto` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 
---
--- Estructura de tabla para la tabla `categoria`
---
 
-CREATE TABLE `categoria` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `categoria`
---
-
-INSERT INTO `categoria` (`id`, `nombre`) VALUES
-(1, 'Categoria 1');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cliente`
---
-
-CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `apellido` varchar(255) NOT NULL,
-  `correo` varchar(255) NOT NULL,
-  `dni` int(11) NOT NULL,
-  `contrasenia` varchar(255) NOT NULL,
-  `domicilio` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `cliente`
---
-
-INSERT INTO `cliente` (`id`, `nombre`, `apellido`, `correo`, `dni`, `contrasenia`, `domicilio`) VALUES
-(1, 'Juan', 'Perez', 'juan@example.com', 123456789, 'contraseña123', '123 Calle Principal'),
-(2, 'Juan', 'Perez', 'juan@example.com', 123456789, 'contraseña123', '123 Calle Principal'),
-(3, 'Juan', 'Perez', 'juan@example.com', 123456789, 'contraseña123', '123 Calle Principal');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pedido`
---
-
-CREATE TABLE `pedido` (
-  `id` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `estado` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `pedido`
---
-
-INSERT INTO `pedido` (`id`, `id_usuario`, `estado`) VALUES
-(11, 1, 'Pendiente'),
-(12, 1, 'Pendiente');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pedido_producto`
---
-
-CREATE TABLE `pedido_producto` (
-  `id` int(11) NOT NULL,
-  `pedido_id` int(11) DEFAULT NULL,
-  `producto_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `pedido_producto`
---
-
-INSERT INTO `pedido_producto` (`id`, `pedido_id`, `producto_id`) VALUES
-(11, 11, 3),
-(12, 11, 4),
-(13, 12, 3),
-(14, 12, 4);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `producto`
---
-
-CREATE TABLE `producto` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `descripcion` text DEFAULT NULL,
-  `precio` decimal(10,2) NOT NULL,
-  `imagen` varchar(255) DEFAULT NULL,
-  `stock` int(11) NOT NULL,
-  `categoria_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `producto`
---
-
-INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `precio`, `imagen`, `stock`, `categoria_id`) VALUES
-(3, 'Producto 1', 'Descripción del Producto 1', '19.99', 'imagen1.jpg', 100, 1),
-(4, 'Producto 1', 'Descripción del Producto 1', '19.99', 'imagen1.jpg', 100, 1),
-(5, 'Producto 1', 'Descripción del Producto 1', '19.99', 'imagen1.jpg', 100, 1);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `pedido`
---
-ALTER TABLE `pedido`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_usuario` (`id_usuario`);
-
---
--- Indices de la tabla `pedido_producto`
---
-ALTER TABLE `pedido_producto`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `pedido_id` (`pedido_id`),
-  ADD KEY `producto_id` (`producto_id`);
-
---
--- Indices de la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `categoria_id` (`categoria_id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `pedido`
---
-ALTER TABLE `pedido`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT de la tabla `pedido_producto`
---
-ALTER TABLE `pedido_producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT de la tabla `producto`
---
-ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `pedido`
---
-ALTER TABLE `pedido`
-  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `cliente` (`id`);
-
---
--- Filtros para la tabla `pedido_producto`
---
-ALTER TABLE `pedido_producto`
-  ADD CONSTRAINT `pedido_producto_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`),
-  ADD CONSTRAINT `pedido_producto_ibfk_2` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`);
-
---
--- Filtros para la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
