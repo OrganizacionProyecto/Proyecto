ac0377671feb8bee1975f35357d915510c7f04fc
Damian-Figueroa <conichan139@gmail.com>
2023-09-20 21:17:10 -0300

    bucle opciones menu

--- Backend/main.py
+++ Backend/main.py
@@ -25,42 +25,29 @@ def main():
     # Conectar a la base de datos
     conexion.conectar()
 
-    try:
-        # Crear un cliente y guardar en la base de datos
-        
-        #cliente1 = Cliente(None, "Pedro", "Gomez", "Pedro@example.com", 123456789, "contraseña123", "123 Calle Principal")
-        #cliente1.registrarCliente(conexion.conexion)
-
-        # Crear una categoria y guardar en la base de datos
-        #categoria1 = Categoria(None, "Categoria 1")
-        #categoria1.registrarCategoria(conexion.conexion)
-
-
-        # Crear un producto y guardar en la base de datos
-        producto1 = Producto(None, "Producto 3", "Descripción del Producto 3", 19.99, "imagen3.jpg", 100, 1)
-        #producto2 = Producto(None, "Producto 4", "Descripción del Producto 4", 29.99, "imagen4.jpg", 200, 1)
-        producto1.registrarProducto(conexion.conexion)
-
-        # Crear un pedido y agregar productos al pedido
-        #pedido1 = Pedido(None, cliente1.id, "Pendiente")
-        #pedido1.agregarProducto(producto1)
-        #pedido1.agregarProducto(producto2)
-        # Guardar el pedido en la base de datos
-        #pedido1.registrarPedido(conexion.conexion)
+    while True:
+        mostrar_menu()
+        op = input("Seleccione una opcion: ")
+        if op == "1":
+            """Gestionar Clientes"""
+            pass
+        if op == "2":
+            """Gestionar Categorias"""
+            pass
+        if op == "3":
+            """Gestionar Producto"""
+            pass
+        if op == "4":
+            """Gestionar Pedidos"""
+            pass
+        if op == "5":
+            """Salir"""
+            break
+        else:
+            print("Opcion no valida, ingrese una que sea valida")
 
 
-
-        # Crear un pedido y agregar un producto al pedido
-        #pedido1 = Pedido(None, cliente1.id, "Pendiente")
-        #pedido1.agregarProducto(producto)
-
-        # Guardar el pedido en la base de datos
-        #pedido1.registrarPedido(conexion.conexion)
-
-    except Exception as e:
-        print(f"Error al interactuar con la base de datos: {str(e)}")
-
-    # Cerrar la conexión a la base de datos
+# Cerrar la conexión a la base de datos
     conexion.cerrar()
 
 if __name__ == "__main__":
