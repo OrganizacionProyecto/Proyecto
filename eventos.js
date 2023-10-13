047345ebe9a93d654cb4282b3f0b106e63494072
Walter <106196739+waltercamino@users.noreply.github.com>
2023-10-13 19:40:41 -0300

    agregue el evento al formulario de login e hice algunas correcciones donde va el scrip de js

--- eventos.js
+++ eventos.js
@@ -1,3 +1,4 @@
+//evento mensaje envio formulario contacto
 
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('.form2');
@@ -8,9 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
       const nombre = document.getElementById('nombre').value;
       const email = document.getElementById('email').value;
   
-      //  agregar la lógica para enviar el correo electrónico.
-  
-      alert("Su solicitud se ha enviado con éxito. En breve nos comunicaremos con usted.");
+    alert("Su solicitud se ha enviado con éxito. En breve nos comunicaremos con usted.");
   
       form.reset();
     });
@@ -19,35 +18,31 @@ document.addEventListener('DOMContentLoaded', function() {
   
  //eventos del formulario de login
 
- document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('showRegister').addEventListener('click', function() {
-      document.getElementById('registerContainer').style.display = 'block';
-      document.getElementById('showLogin').style.display = 'inline';
-      document.getElementById('showRegister').style.display = 'none';
-      document.getElementById('loginContainer').style.display = 'none';
+ document.getElementById('formLogin').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+  
+    var nomUsuario = document.getElementById('nombreUsuario').value;
+    var contraseña = document.getElementById('password').value;
+  
+    //variables ficticias para prueba
+    var usuarios = [
+      {nomUsuario: 'usuario1', contraseña: 'contraseña1'},
+      {nomUsuario: 'usuario2', contraseña: 'contraseña2'},
+    ];
+  
+    var usuarioCorrecto = false;
+  
+    for (var i = 0; i < usuarios.length; i++) {
+      if (usuarios[i].nomUsuario === nomUsuario && usuarios[i].contraseña === contraseña) {
+        usuarioCorrecto = true;
+        break;
+      }
+    }
+  
+    if (usuarioCorrecto) {
+      alert('Inicio de sesión exitoso');
+    } else {
+      alert('El nombre de usuario y/o la contraseña no son correctos.');
+    }
   });
-
-  document.getElementById('showLogin').addEventListener('click', function() {
-      document.getElementById('registerContainer').style.display = 'none';
-      document.getElementById('showLogin').style.display = 'none';
-      document.getElementById('showRegister').style.display = 'inline';
-      document.getElementById('loginContainer').style.display = 'block';
-  });
-
-  document.getElementById('loginForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-      var username = document.getElementById('username').value;
-      var password = document.getElementById('password').value;
-      // Aquí puedes agregar la lógica de autenticación con JavaScript
-  });
-
-  document.getElementById('registerForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-      var regUsername = document.getElementById('regUsername').value;
-      var regPassword = document.getElementById('regPassword').value;
-      var regEmail = document.getElementById('regEmail').value;
-      // Aquí puedes agregar la lógica de registro con JavaScript
-  });
-});
-
-
+  
