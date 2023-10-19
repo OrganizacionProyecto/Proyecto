ac9451c495b9d859e0956f56763517479a053c23
AntonellaAc <106637867+AntonellaAc@users.noreply.github.com>
2023-10-18 23:28:53 -0300

    agregue evento al formulario de registro

--- eventos.js
+++ eventos.js
@@ -46,3 +46,51 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
   
+  document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('.form2');
+  
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+  
+      const nombre = document.getElementById('nombre').value;
+      const email = document.getElementById('email').value;
+  
+    alert("Su solicitud se ha enviado con éxito. En breve nos comunicaremos con usted.");
+  
+      form.reset();
+    });
+  });
+
+  
+ //eventos del formulario de Registro
+
+ document.getElementById('registroForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+  
+    var regNombreUsuario = document.getElementById('regNombreUsuario').value;
+    var contraseña = document.getElementById('regPassword').value;
+    var mail= document.getElementById('regEmail').value;
+  
+    //variables ficticias para prueba
+    var usuarios = [
+      {regNombreUsuario: 'usuario1', contraseña: 'contraseña1', mail: 'usuario1@gmail.com'},
+      {regNombreUsuario: 'usuario2', contraseña: 'contraseña2', mail: 'usuario2@gmail.com'},
+    ];
+  
+    var usuarioCorrecto = false;
+  
+    for (var i = 0; i < usuarios.length; i++) {
+      if (usuarios[i].regNombreUsuario === regNombreUsuario && usuarios[i].contraseña === contraseña && 
+        usuarios[i].mail=== mail) {
+        usuarioCorrecto = true;
+        break;
+      }
+    }
+  
+    if (usuarioCorrecto) {
+      alert('Registro exitoso');
+    } else {
+      alert('Los datos ingresados son incorrectos.');
+    }
+  });
+  
