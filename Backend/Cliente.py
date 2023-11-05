74e3f2946fdee1193498e37b7f46e86246d5c2e0
Damian <82007987+Conichan@users.noreply.github.com>
2023-11-05 18:12:55 -0300

    Cliente.py
    actualizacion cliente

--- Backend/Cliente.py
+++ Backend/Cliente.py
@@ -1,4 +1,4 @@
-import Pedido
+from Pedido import Pedido
 import mysql.connector
 from Usuario import Usuario
 
@@ -25,10 +25,13 @@ class Cliente(Usuario):
     def registrarCliente(self, conexion):
         try:
             cursor = conexion.obtener_cursor()
+
+            # Solicitar al usuario los datos del producto
+            # Convierte la entrada a un valor numérico
             
-            # Define la sentencia SQL para insertar un cliente en la base de datos
+            # Define la sentencia SQL para insertar un nuevo producto
             sql = """
-            INSERT INTO Cliente (dni, id_usuario)
+            INSERT INTO cliente (dni, id_usuario)
             VALUES (%s, %s)
             """
 
@@ -44,10 +47,41 @@ class Cliente(Usuario):
             # Confirma los cambios en la base de datos
             conexion.conexion.commit()
 
-            print("Cliente insertado en la base de datos")
+            print("Producto insertado en la base de datos")
 
         except mysql.connector.Error as error:
-            print(f"Error al insertar cliente en la base de datos: {error}")
+            print(f"Error al insertar producto en la base de datos: {error}")
+
+    def eliminarCliente(self,conexion):
+        try:
+            cursor = conexion.cursor()
+
+            # Define la sentencia SQL para actualizar un cliente en la base de datos
+            sql = """
+            UPDATE Cliente
+            SET nombre = %s, apellido = %s, correo = %s, domicilio = %s
+            WHERE id = %s
+            """
+
+            # Valores a actualizar
+            valores = (
+                nombre if nombre is not None else self.nombre,
+                apellido if apellido is not None else self.apellido,
+                correo if correo is not None else self.correo,
+                domicilio if domicilio is not None else self.domicilio,
+                self.id
+            )
+
+            # Ejecuta la sentencia SQL para actualizar el cliente
+            cursor.execute(sql, valores)
+
+            # Confirma los cambios en la base de datos
+            conexion.conexion.commit()
+
+            print("Producto insertado en la base de datos")
+
+        except mysql.connector.Error as error:
+            print(f"Error al insertar producto en la base de datos: {error}")
 
     def eliminarCliente(self,conexion):
         try:
@@ -68,7 +102,25 @@ class Cliente(Usuario):
             print("Cliente eliminado de la base de datos")
 
         except mysql.connector.Error as error:
-            print(f"Error al eliminar categoria de la base de datos: {error}")
+            print(f"Error al eliminar cliente de la base de datos: {error}")
 
+    def mostrarCliente(self):
+        return f"{self.nombre} {self.apellido}"
 
+    def realizarPedido(self, productos, cantidad):
+        # Implementa la lógica para que un cliente realice un pedido.
+        # Puedes crear una instancia de la clase Pedido y agregarla a la lista de pedidos del cliente.
+        nuevo_pedido = Pedido(self, productos, cantidad)
+        self.pedidos.append(nuevo_pedido)
+
+    def cambiarContrasenia(self, nueva_contrasenia):
+        # Implementa la lógica para que el cliente cambie su contraseña.
+        self.contrasenia = nueva_contrasenia
+
+    def iniciarSesion(self, correo, contrasenia):
+        # Implementación para iniciar sesión de un cliente
+        if self.correo == correo and self.contrasenia == contrasenia:
+            return True
+        else:
+            return False
 
